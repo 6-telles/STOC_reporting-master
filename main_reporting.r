a8e9b5f823459a969251c0f528af96262419b67c
Romain Lorrilliere <romainlorrilliere@gmail.com>
2020-04-29 15:22:24 +0200

    Create main_reporting.r

---
+++ main_reporting.r
@@ -0,0 +1,5 @@
+source("functions/fun_generic_latin1.r")
+source("functions/fun_indicateurs_france_latin1.r")
+source("functions/fun_indicateurs_local_latin1.r")
+source("functions/fun_rmd_latin1.r")
+source("functions/fun_mainSTOCreporting_latin1.r")
