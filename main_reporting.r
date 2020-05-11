b26c31f07ce1338b4929985435aeef9672527046
Romain Lorrilliere <romainlorrilliere@gmail.com>
2020-05-11 15:42:55 +0200

    Update main_reporting.r

--- main_reporting.r
+++ main_reporting.r
@@ -3,3 +3,5 @@ source("functions/fun_indicateurs_france_latin1.r")
 source("functions/fun_indicateurs_local_latin1.r")
 source("functions/fun_rmd_latin1.r")
 source("functions/fun_mainSTOCreporting_latin1.r")
+source("functions/fun_stoc_preparation_data_latin1.r")
+
