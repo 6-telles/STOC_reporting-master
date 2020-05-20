f9b754cd9f877eb9b6c9ba7ea178150ef5830d79
Romain Lorrilliere <romainlorrilliere@gmail.com>
2020-05-20 20:15:36 +0200

    Update fun_rmd_latin1.r

--- functions/fun_rmd_latin1.r
+++ functions/fun_rmd_latin1.r
@@ -5,7 +5,7 @@
 ## source("functions/fun_generic_latin1.r")
 source("functions/fun_indicateurs_local_latin1.r")
 
-vecPackage=c("data.table","devtools","dplyr","ggplot2","ggpubr","lubridate","reshape2","knitr","kableExtra","pander")
+vecPackage=c("data.table","devtools","dplyr","ggplot2","ggpubr","lubridate","reshape2","knitr","kableExtra","pander","stringr")
 
 ip <- installed.packages()[,1]
 
@@ -17,15 +17,22 @@ for(p in vecPackage) {
 
 
 
-run.rmd <- function(file.rmd="functions/rmd_stoc_reporting_utf8.rmd",rep.out="output_rapport_pdf",file.data="data_DB/data3session.csv",file.out=NULL,year = NULL, id_station = "205",format_output="pdf",prefixe_file="",fileLog =NULL,save.fig=FALSE){
+run.rmd <- function(file.rmd="functions/rmd_stoc_reporting_utf8.rmd",rep.out="output_rapport_pdf",file.data="data_DB/data3session.csv",file.out=NULL,year = NULL, id_station = "205",format_output="pdf",prefixe_file="",fileLog =NULL,save.fig=FALSE,render.clean=TRUE){
 
 
 #file.rmd="functions/rmd_stoc_reporting_utf8.rmd";rep.out="output_rapport_pdf";file.data="data_DB/data3session.csv";file.out=NULL;year = 2018; id_station = "205";format_output="pdf"
 
 
+    id_station_num <-  gsub("[a-z]","",id_station)
+    id_station_letter <-  gsub(id_station_num,"",id_station)
+    if(nchar(id_station_letter) == 0)id_station_letter <- "_"
+    id_station_num <- str_pad(id_station_num, 4, pad = "0")
+    id_station_txt <- paste0(id_station_num,id_station_letter)
+
     if(is.null(year)) year <- as.numeric(substr(Sys.time(),1,4))
-    if(is.null(prefixe_file)) prefixe_file <- paste0(format(Sys.time(),"%Y-%m-%d_%H-%M"),"_")
-    if(is.null(file.out)) file.out <- paste0("stoc_reporting_",prefixe_file,year,"_",id_station,".",format_output)
+    if(is.null(prefixe_file)) prefixe_file <- format(Sys.time(),"%Y-%m-%d_%H-%M")
+    if(prefixe_file != "") prefixe_file <- paste0(prefixe_file,"_")
+    if(is.null(file.out)) file.out <- paste0("stoc_reporting_",prefixe_file,"station_",  id_station_txt,"_",year,".",format_output)
     if(!is.null(rep.out)) file.out <- paste0(rep.out,"/",file.out)
      format <- paste0(format_output,"_document")
 
@@ -42,7 +49,7 @@ run.rmd <- function(file.rmd="functions/rmd_stoc_reporting_utf8.rmd",rep.out="ou
          cat("\n Répertoire créer !!\n")
      }
 
-      rmarkdown::render(file.rmd,output_file=file.out,output_dir=rep.out,output_format = format,clean=FALSE,encoding="utf-8",params = list(set_rep = rep, set_file_data = file.data,set_fileLog = fileLog,set_site = id_station, set_year = year, set_save_fig = save.fig))
+      rmarkdown::render(file.rmd,output_file=file.out,output_dir=rep.out,output_format = format,clean=render.clean,encoding="utf-8",params = list(set_rep = rep, set_file_data = file.data,set_fileLog = fileLog,set_site = id_station, set_year = year, set_save_fig = save.fig))
 
      cat("DONE !!!\n")
 }
